fca623221f3077e1da935f88578fb590f07f0c0f
Tagayasu <ig7dpop0kbz6@gmail.com>
2020-07-13 22:13:58 +0900

    http status codeの種類増やした

--- Sources/WebCLI/WebResponse.swift
+++ Sources/WebCLI/WebResponse.swift
@@ -10,14 +10,27 @@ import Foundation
 public typealias WebReqestResult = Result<WebResponse, APIError>
 
 public enum HTTPStatus {
-    case ok
+    case info(code: Int)
+    case ok(code: Int)
+    case redirect(code: Int)
+    case clientError(code: Int)
+    case serverError(code: Int)
+    case unknown
     
     init(statusCode: Int) {
         switch statusCode {
-        case 200:
-            self = .ok
+        case 100...199:
+            self = .info(code: statusCode)
+        case 200...299:
+            self = .ok(code: statusCode)
+        case 300...399:
+            self = .redirect(code: statusCode)
+        case 400...499:
+            self = .clientError(code: statusCode)
+        case 500...599:
+            self = .serverError(code: statusCode)
         default:
-            self = .ok
+            self = .unknown
         }
     }
 }
